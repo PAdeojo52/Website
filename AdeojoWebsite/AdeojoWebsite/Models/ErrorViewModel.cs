6312b2466723b0620dc31be011619563ef4574ed
Peter Adeojo <PAdeojo@ggc.edu>
2020-01-18 22:02:42 -0500

    WebSite
    init commit

---
+++ AdeojoWebsite/AdeojoWebsite/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace AdeojoWebsite.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
